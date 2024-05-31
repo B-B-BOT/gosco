8d0a0ea3d29e10110d7638523c8c6b631fc022e5
Raymond-188 <raysberry44@gmail.com>
2024-05-31 17:24:03 +0100

    Update All

--- bicot/settings.py
+++ bicot/settings.py
@@ -123,7 +123,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-PORT = os.getenv('PORT', '8000')
+
 
 
 # Default primary key field type
